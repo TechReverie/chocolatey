5eb34bcda2e6c9b46493e45b84138d232d49bf8f
Jim <1884211+jim1138@users.noreply.github.com>
2021-03-18 14:48:20 +0000

    change from alias command for clarity of example

--- install-chocolatey.ps1
+++ install-chocolatey.ps1
@@ -1,4 +1,4 @@
 # Install Chocolatey
 # Firstly, ensure execution policy is not Restricted
 # use "Bypass" or "AllSigned" to get things installed and then remember to amend as suitable.
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
